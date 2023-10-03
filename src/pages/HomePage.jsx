108263b9d1417d2f682faf0791c52fda0174113b
Leonel Ledesma <leonelledesma.dev@outlook.com>
2023-10-02 23:46:38 -0300

    Se crea carousel en Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CarouselHome from '../components/Carousel'
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
-    </div>
+    <>
+    <CarouselHome/>
+    </>
   )
 }
 
