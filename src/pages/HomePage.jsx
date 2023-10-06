7f1fb748298be4bd5ec45601073d49db3a930f56
unknown <bazanjuanjordan@gmail.com>
2023-10-06 19:25:47 -0300

    se modifica cadsServicios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import CarouselHome from '../components/Carousel'
-import CardServ from '../components/CardServ'
-import CardComp from '../components/CardComp'
+import Cards from '../components/Cards'
 
 const HomePage = () => {
   return (
     <>
     <CarouselHome/>
    
-      <CardComp/>
-      <hr />
-    <div className="container">
-      <div className="row">
-      <div className='d-flex'><h3>Servicios</h3></div>
-      </div>
-    </div>
-      <CardServ/>
+      <Cards/>
+
      
     </>
   )
